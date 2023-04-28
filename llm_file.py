46b15d6442d0f687692c0d544daddf0327c6d464
Dhanush <107381860+Dhanush0071@users.noreply.github.com>
2023-04-28 13:20:02 +0530

    Update llm_file.py

--- llm_file.py
+++ llm_file.py
@@ -7,11 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1rjH0hY8Hx2QwW-yJ082pqD5NZJ_SAbVW
 """
 
-import requests
-import webbrowser
-import io
-import PyPDF2
-import os
 import streamlit as st
 from streamlit_chat import message
 from langchain.document_loaders import OnlinePDFLoader
